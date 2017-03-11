59827fed1702cb19e9595e34eef18cd043235290
RedBeren <zbmerritt@gmail.com>
2017-03-11 12:13:56 -0500

    Markov chain visualization

--- clickstream.R
+++ clickstream.R
@@ -19,9 +19,14 @@ abs_states_dur <- absorbingStates(mc_dur)
 
 # Use igraph package to plot chain
 net <- graph_from_adjacency_matrix(as.matrix(mc@transitions$`1`), mode = 'directed', weighted = TRUE)
-net <- delete.edges(net, which(E(net)$weight <= 0.1))
+net <- delete.edges(net, which(E(net)$weight <= 0.4))
 net <- delete.vertices(net,which(degree(net)<1))
-plot(net)
+net <- simplify(net, remove.loops = T)
+E(net)$arrow.size <- 0.2
+deg <- degree(net, mode = 'all')
+colrs <- ifelse(deg > 3, 'red','blue')
+plot(net, vertex.shape = 'none',
+     vertex.label.color = colrs)
 
 # Two main absorbing states, so try splitting into two clusters
 clusters <- clusterClickstreams(clicks, order = 0, centers = 2)
@@ -30,8 +35,8 @@ summary(clusters)
 # Build Markov Chain off each cluster and check absorbing states
 mc1 <- fitMarkovChain(clusters$clusters[1])
 mc2 <- fitMarkovChain(clusters$clusters[2])
-show(mc1) # Code 15001 Warning NoStop
-show(mc2) # Code 10105 Stop Urgency5
+show(mc1) # Code 10105 Stop Urgency5
+show(mc2) # Code 14001 Warning NoStop
 
 #Frequencies
 frequencyDF <- frequencies(clicks)
